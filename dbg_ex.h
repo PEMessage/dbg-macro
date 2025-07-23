750878ca7528274c6211c658bd3cc138163fe3cf
PEMessage <1165739182@qq.com>
2025-07-23 23:33:02 +0800

    add dbg_ex.h to support bitwizeshift/result, base on std::pair

---
+++ dbg_ex.h
@@ -0,0 +1,58 @@
+#ifndef DBG_MACRO_DBG_EX_H
+#define DBG_MACRO_DBG_EX_H
+#include "result.hpp"
+#include <string>
+
+namespace dbg {
+// These forward declaration must be declare before dbg.h included
+template <typename T, typename E>
+inline bool pretty_print(std::ostream& stream, const cpp::result<T,E>& value);
+
+template <typename E>
+inline bool pretty_print(std::ostream& stream, const cpp::failure<E>& value);
+}
+
+#include "dbg.h"
+
+namespace dbg {
+// Add type_name specializations for result and failure
+template <typename T, typename E>
+std::string get_type_name(type_tag<cpp::result<T,E>>) {
+    return "result<" + type_name<T>() + ", " + type_name<E>() + ">";
+}
+
+template <typename E>
+std::string get_type_name(type_tag<cpp::failure<E>>) {
+    return "failure<" + type_name<E>() + ">";
+}
+
+template <typename T, typename E>
+inline bool pretty_print(std::ostream& stream, const cpp::result<T,E>& value) {
+    if (value.has_value()) {
+        if constexpr (!std::is_void_v<T>) {
+            stream << "value: ";
+            pretty_print(stream, *value);
+        } else {
+            (void)(value);
+            stream << "void";
+        }
+    } else {
+        stream << "result{error: ";
+        pretty_print(stream, value.error());
+    }
+    stream << "}";
+    return true;
+}
+
+template <typename E>
+inline bool pretty_print(std::ostream& stream, const cpp::failure<E>& value) {
+    stream << "failure{";
+    pretty_print(stream, value.error());
+    stream << "}";
+    return true;
+}
+
+
+} // namespace dbg
+#endif // #ifndef DBG_MACRO_DBG_H
+
